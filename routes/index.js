25272433b8f26f648b22e768501e1a7be25418cf
chuang57 <christiandhuang7@gmail.com>
2022-03-30 23:54:01 -0400

    updated constructorMethod

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ const path = require('path');
 
 const constructorMethod = (app) => {
   app.use('/', showRoutes);
-  app.get('/about', (req, res) => {
-    res.sendFile(path.resolve('static/about.html'));
-  });
-
+ 
   app.use('*', (req, res) => {
     res.status(400).json({ error: 'Not found' });
   });
